bd96863c45c99671c9b838c9366faa24ec9557c5
Jennifer von Trotta-Treyden <jv222th@student.lnu.se>
2023-12-28 04:49:18 +0100

    Fix so that the app windows are draggable and that the clicked window is at the front

--- src/js/index.js
+++ src/js/index.js
@@ -13,8 +13,11 @@ const main = document.querySelector('main')
 const memoryIcon = document.querySelector('#memory')
 const chatIcon = document.querySelector('#chat')
 const customIcon = document.querySelector('#custom')
+let appCounter = 0
 
 memoryIcon.addEventListener('click', () => {
+  appCounter += 1
+  // Create all the neccessary elements and add classes.
   const app = document.createElement('div')
   const appBar = document.createElement('div')
   const exit = document.createElement('p')
@@ -23,13 +26,70 @@ memoryIcon.addEventListener('click', () => {
   appBar.classList.add('appBar')
   app.classList.add('app')
 
+  // Make the appBar draggable.
+  appBar.setAttribute('draggable', 'true')
+
+  // Bring all the elements together.
   appBar.append(exit)
   app.append(appBar)
   app.append(document.createElement('memory-game'))
   main.append(app)
 
+  // Make the 'X' remove the app.
   exit.addEventListener('click', () => {
     main.removeChild(app)
+  })
+
+  let initialX
+  let initialY
+
+  // Handle the start of the drag.
+  appBar.addEventListener('dragstart', (event) => {
+    // Store the initial position relative to the cursor
+    const appBarRect = appBar.getBoundingClientRect()
+    initialX = event.clientX - appBarRect.left
+    initialY = event.clientY - appBarRect.top
+
+    // Set the data being dragged.
+    event.dataTransfer.setData('text/plain', '')
+    event.dataTransfer.effectAllowed = 'none'
+
+    app.style.zIndex = appCounter++
+  })
+
+  // Handle the active drag event.
+  appBar.addEventListener('drag', (event) => {
+    const x = event.clientX - initialX
+    const y = event.clientY - initialY
+
+    // Set the new position of the app.
+    app.style.position = 'absolute'
+    app.style.left = `${x}px`
+    app.style.top = `${y}px`
+  })
+
+  // Handle the end of the dragging.
+  appBar.addEventListener('dragend', (event) => {
+    const x = event.clientX - initialX
+    const y = event.clientY - initialY
+
+    app.style.position = 'absolute'
+    app.style.left = `${x}px`
+    app.style.top = `${y}px`
+  })
+
+  // Prevent default drag-and-drop behaviour.
+  appBar.addEventListener('dragenter', (event) => {
+    event.preventDefault()
+    event.dataTransfer.dropEffect = 'none'
+  })
+  appBar.addEventListener('dragover', (event) => {
+    event.preventDefault()
+    event.dataTransfer.dropEffect = 'none'
+  })
+
+  app.addEventListener('click', () => {
+    app.style.zIndex = appCounter++
   })
 })
 
@@ -41,6 +101,7 @@ chatIcon.addEventListener('click', () => {
   exit.classList.add('exit')
   appBar.classList.add('appBar')
   app.classList.add('app')
+  app.setAttribute('draggable', 'true')
 
   appBar.append(exit)
   app.append(appBar)
@@ -60,6 +121,7 @@ customIcon.addEventListener('click', () => {
   exit.classList.add('exit')
   appBar.classList.add('appBar')
   app.classList.add('app')
+  app.setAttribute('draggable', 'true')
 
   appBar.append(exit)
   app.append(appBar)
