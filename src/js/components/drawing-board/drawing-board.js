2922d9a97a29162cf3e6b9ef24b9e20edd43fd60
Jennifer von Trotta-Treyden <jv222th@student.lnu.se>
2023-12-31 00:53:12 +0100

    Add event listeners for the drawing actions and start declaring methods with JSdoc

--- src/js/components/drawing-board/drawing-board.js
+++ src/js/components/drawing-board/drawing-board.js
@@ -32,7 +32,7 @@ ${drawingBoardStyles}
             <button class="color" id="purple"></button>
         </div>
         <div id="eraser">
-            <button id="eraserBtn"></button>
+            <button class="color" id="eraserBtn"></button>
         </div>
     </div>
         <div id="canvas"></div>
@@ -57,6 +57,10 @@ customElements.define('drawing-board',
       this.canvas = this.shadowRoot.querySelector('#canvas')
       this.colors = this.shadowRoot.querySelectorAll('.color')
       this.brushes = this.shadowRoot.querySelectorAll('.brush')
+      this.eraser = this.shadowRoot.querySelector('#eraserBtn')
+
+      this.isDrawing = false
+      this.context = this.canvas.getContext('2d')
 
       this.colors.forEach((color) => {
         color.addEventListener('click', (event) => this.handleColorPicker(event))
@@ -65,5 +69,28 @@ customElements.define('drawing-board',
       this.brushes.forEach((brush) => {
         brush.addEventListener('click', (event) => this.handleBrushPicker(event))
       })
+
+      this.canvas.addEventListener('mousedown', (event) => this.startDrawing(event))
+      this.canvas.addEventListener('mousemove', (event) => this.draw(event))
+      this.canvas.addEventListener('mouseup', () => this.stopDrawing())
+      this.canvas.addEventListener('mouseleave', () => this.stopDrawing())
+    }
+
+    /**
+     * Handles the choice of brush size.
+     *
+     * @param {Event} event - When the button is clicked.
+     */
+    handleBrushPicker (event) {
+      event.preventDefault()
+    }
+
+    /**
+     * Handles the choice of color.
+     *
+     * @param {Event} event - When the button is clicked.
+     */
+    handleColorPicker (event) {
+      event.preventDefault()
     }
   })
